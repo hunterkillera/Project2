fa0d66a6b40b0a55d1dceb43e92d9f4c78fec48d
Matt Cole <ccmatt19@gmail.com>
2019-04-11 23:15:54 -0400

    This file connects to Spotify's API and searches for a song

--- spotify_connection.py
+++ spotify_connection.py
@@ -3,23 +3,23 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import pprint
 
 def main():
-    # Connection information
+    # Project-specific connection information
     client_id = '4394f6c6ce164881abb7cf4b46aa7c2b'
-    client_secret = '838d2b7c59804892a71d4d26859f7ec3'
+    client_secret = '838d2b7c59804892a71d4d26859f7ec3' #TODO: Hide this
 
+    # Connect to spotify's API
     credentials = SpotifyClientCredentials(client_id, client_secret)
     spotify = spotipy.Spotify(client_credentials_manager=credentials)
 
-    # results = spotify.search('Mona Lisa', type='track', limit=1)
-    # artists = results['tracks']['items'][0]['album']['artists']
-    # pprint.pprint(artists)
-
-
+    # An example search (this essentially returns the same results if you searched 'Look Alive' on their website)
     results = spotify.search('Look Alive', type='track', limit=1)
+
+    # Cleans up the results to show the artists that are in it
     artists = results['tracks']['items'][0]['album']['artists']
 
-    #pprint.pprint(results)
+    # Pretty print the results because the JSON object is ugly and confusing
     pprint.pprint(artists)
 
 
-main()
+if __name__ == '__main__':
+    main()
